16e6760d9ef79d61351315e0f27909502744eb3d
Esteban Mendez <mendez01@allegheny.edu>
2022-11-08 11:31:46 -0500

    First attempt on the tiers

--- ReadWriteGoogleSheet.py
+++ ReadWriteGoogleSheet.py
@@ -1,14 +1,30 @@
 import gspread
-from ouath2client.service_account import ServiceAccountCredentials
+import os
+from oauth2client.service_account import ServiceAccountCredentials
 
 scopes = [
     'https://www.googleapis.com/auth/spreadsheets',
     'https://www.googleapis.com/auth/drive'
 ]
 
-creds = ServiceAccountCredentials.from_json_keyfile_name(TODO: Absolute path, scopes=scopes)
+creds = ServiceAccountCredentials.from_json_keyfile_name(os.getcwd()+'/secret_key/key.json', scopes=scopes)
 
 file = gspread.authorize(creds)
 workbook = file.open("Tier-comparison")
 sheet = workbook.sheet1
 
+for cell in sheet.range('C2:C5'):
+    if int(cell) > 90:
+        print("green")
+        print(int(cell).value)
+        print("-----------------")
+    elif int(cell) < 60:
+        print("red")
+        print(int(cell).value)
+        print("-----------------")
+    elif 90 > int(cell) > 70:
+        print("yellow")
+        print(int(cell).value)
+        print("-----------------")
+    else:
+        pass
