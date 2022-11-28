6d48a2fd7bd28c4815abe58c9c1e909b898b856f
Esteban Mendez <mendez01@allegheny.edu>
2022-11-28 18:55:21 -0500

    New files

--- plugin/github_issues.py
+++ plugin/github_issues.py
@@ -1,6 +1,6 @@
 from sheetshuttle import github_interaction
 
-my_manager = github_interaction.GitHubManager()
+my_manager = github_interaction.GithubManager()
 my_manager.collect_config()
 
 # All collected entries can be posted at once
@@ -8,5 +8,5 @@ my_manager.post_all()
 
 # OR they can be posted individually by type
 my_manager.post_issues()
-my_manager.post_pull_requests()
-my_manager.post_files()
+# my_manager.post_pull_requests()
+# my_manager.post_files()
