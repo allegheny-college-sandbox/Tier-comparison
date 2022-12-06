9a16ed40b278fc6328832151185dc834c804f131
Esteban Mendez <mendez01@allegheny.edu>
2022-12-06 13:43:42 -0500

    Fixed bugs with highlight

--- ReadWriteGoogleSheet.py
+++ ReadWriteGoogleSheet.py
@@ -18,21 +18,27 @@ grades = 'C2:C5'
 
 # Separate the grades in tiers by comparing with numbers
 
+# worksheet = some_spreadsheet.worksheet("Tier-comparison")
+
 # colors:
-green = cellFormat(backgroundColor=color(0, 1, 0))
-yellow = cellFormat(backgroundColor=color(1, 1, 0))
-red = cellFormat(backgroundColor=color(1, 0, 0))
+# green = cellFormat(backgroundColor=color(0, 1, 0))
+# yellow = cellFormat(backgroundColor=color(1, 1, 0))
+# red = cellFormat(backgroundColor=color(1, 0, 0))
 
 # What type of data is this? sheet.range(grades), it's probably an iterable.
 # What type of data is cell?
 # If it's an object, what do you need to call to get the value
 
+# sheet.format(f"C{cell.row}",{"textFormat": {"foregroundColor": {"red": 0.0,"green": 1.0,"blue": 0.0}}})
+
 def greentier():
     for cell in sheet.range(grades):
         if 100 > int(cell.value) and int(cell.value) > 90:
+            print("-----------------")
             print("green")
-            (workbook,[(cell, green)])
             print(cell.value)
+            print((sheet.cell(cell.row,cell.col+1)).value)
+            sheet.format(f"C{cell.row}",{"backgroundColor": {"red": 0.0,"green": 1.0,"blue": 0.0}})
             print("-----------------")
         else:
             pass
@@ -41,8 +47,9 @@ def yellowtier():
     for cell in sheet.range(grades):
         if 90 > int(cell.value) and int(cell.value) > 70:
             print("yellow")
-            (workbook,[(cell, yellow)])
             print(cell.value)
+            print((sheet.cell(cell.row,cell.col+1)).value)
+            sheet.format(f"C{cell.row}",{"backgroundColor": {"red": 1.0,"green": 1.0,"blue": 0.0}})
             print("-----------------")
         else:
             pass
@@ -51,8 +58,9 @@ def redtier():
     for cell in sheet.range(grades):
         if 60 > int(cell.value) and int(cell.value) > 0:
             print("red")
-            (workbook,[(cell, red)])
             print(cell.value)
+            print((sheet.cell(cell.row,cell.col+1)).value)
+            sheet.format(f"C{cell.row}",{"backgroundColor": {"red": 1.0,"green": 0.0,"blue": 0.0}})
             print("-----------------")
         else:
             pass
