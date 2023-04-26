dc17e0932fe07dc6abe5db69234711d229dc2d55
EstebanMendez01 <70494570+EstebanMendez01@users.noreply.github.com>
2023-04-26 14:55:20 -0400

    Update github_issues.py

--- plugin/github_issues.py
+++ plugin/github_issues.py
@@ -1,6 +1,8 @@
 from sheetshuttle import github_interaction
+import os
 
-my_manager = github_interaction.GithubManager(key_file="plugin/key.json", sources_dir="config/actually_running")
+my_manager = github_interaction.GithubManager(os.getenv(ISSUES))
+# key_file="plugin/key.json", sources_dir="config/actually_running"
 my_manager.collect_config()
 print("collected config")
 # All collected entries can be posted at once
